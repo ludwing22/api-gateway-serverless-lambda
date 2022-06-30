e46cc7b52f953894c19dcd694e8a243e20a8b5e5
Paulo Henrique Borges <paulo2005ph@gmail.com>
2022-06-29 21:51:48 -0300

    Ajustes no log do projeto e detalhamento do README

--- handler.js
+++ handler.js
@@ -1,13 +1,13 @@
-const AWS = require("aws-sdk");
-const express = require("express");
-const serverless = require("serverless-http");
+const AWS = require("aws-sdk")
+const express = require("express")
+const serverless = require("serverless-http")
 
-const app = express();
+const app = express()
 
-const USERS_TABLE = process.env.USERS_TABLE;
-const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
+const USERS_TABLE = process.env.USERS_TABLE
+const dynamoDbClient = new AWS.DynamoDB.DocumentClient()
 
-app.use(express.json());
+app.use(express.json())
 
 app.get("/users/:userId", async function (req, res) {
   const params = {
@@ -15,30 +15,36 @@ app.get("/users/:userId", async function (req, res) {
     Key: {
       userId: req.params.userId,
     },
-  };
+  }
 
   try {
-    const { Item } = await dynamoDbClient.get(params).promise();
+    const { Item } = await dynamoDbClient.get(params).promise()
     if (Item) {
-      const { userId, name } = Item;
-      res.json({ userId, name });
+      const { userId, name } = Item
+
+      console.log('Usuário criado com sucesso.')
+
+      res.json({ userId, name })
     } else {
+      console.error('Usuário não encontrado.e')
       res
         .status(404)
-        .json({ error: 'Could not find user with provided "userId"' });
+        .json({ error: 'Não foi encontrado usuário com este id' })
     }
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Could not retreive user" });
+    console.error(error)
+    res.status(500).json({ error: "Não foi possível obter o usuário." })
   }
-});
+})
 
 app.post("/users", async function (req, res) {
-  const { userId, name } = req.body;
+  const { userId, name } = req.body
   if (typeof userId !== "string") {
-    res.status(400).json({ error: '"userId" must be a string' });
+    console.error('O campo deverá ser uma string.')
+    res.status(400).json({ error: 'O campo deverá ser uma string.' })
   } else if (typeof name !== "string") {
-    res.status(400).json({ error: '"name" must be a string' });
+    console.error('O nome deverá ser uma string.')
+    res.status(400).json({ error: 'O nome deverá ser uma string.' })
   }
 
   const params = {
@@ -47,22 +53,23 @@ app.post("/users", async function (req, res) {
       userId: userId,
       name: name,
     },
-  };
+  }
 
   try {
-    await dynamoDbClient.put(params).promise();
-    res.json({ userId, name });
+    await dynamoDbClient.put(params).promise()
+    console.log('Usuário criado com sucesso.')
+    res.json({ userId, name })
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Could not create user" });
+    console.error(error)
+    res.status(500).json({ error: "Não foi possível criar o usuário" })
   }
-});
+})
 
 app.use((req, res, next) => {
   return res.status(404).json({
     error: "Not Found",
-  });
-});
+  })
+})
 
 
-module.exports.handler = serverless(app);
+module.exports.handler = serverless(app)
